af009043ee0817a3c07551abbadb57c10e84b6df
Arunbalaji <arunbalajimanickam@gmail.com>
2024-12-11 18:38:40 +0530

    feat:automated frames

---
+++ frames_automation.py
@@ -0,0 +1,31 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import Select
+import time
+
+# Ensure the path to your ChromeDriver executable is correct
+chrome_driver_path = r'D:\ARUN\Selenium_learning\selenium_python\chromedriver-win64\chromedriver.exe'
+service = Service(chrome_driver_path)
+
+# Initialize the WebDriver with the service
+driver = webdriver.Chrome(service=service)
+# Open the working website
+driver.get("https://the-internet.herokuapp.com/")
+time.sleep(3)
+#opening the frames
+element = driver.find_element(By.LINK_TEXT, "Frames")
+element.click()
+time.sleep(2)
+#opening the nested frames
+element = driver.find_element(By.LINK_TEXT, "Nested Frames")
+element.click()
+time.sleep(2)
+
+
+# Switch to framecle
+driver.switch_to.frame("frame-bottom")
+print("Inside Frame")
+driver.switch_to.default_content()  # Switch back to main content
+print("Outside Frame")
+driver.quit()
